d80a72e7a89eea593ac2fcb0be7679f38e0adb10
TylerShax <247200418+TylerShax@users.noreply.github.com>
2026-03-15 16:04:40 +0100

    Began refactoring for manual aligned alloc

--- main.cpp
+++ main.cpp
@@ -1,62 +1,61 @@
 #include <vector>
+#include <malloc.h>
 #include "graphics.hpp"
 #include "particle.hpp"
 #include "physics.hpp"
 
-void spawnAlignedParticle(std::vector<dynamics::particle>& collective, vec2::Vector2f center, int count, float radius);
+void SpawnAlignedParticles(std::vector<float>& positionsX, std::vector<float>& positionsY, std::vector<float>& OLDpositionsX, std::vector<float>& OLDpositionsY);
 
 int main() {
-
     graphics::initWindow();
 
-    // PARTICLE INIT
-    constexpr short unsigned int particleCount = 100000;
+    float *positionsX = (float*)_aligned_malloc(physics::particleCount * sizeof(float), 64);
+    float *positionsY = (float*)_aligned_malloc(physics::particleCount * sizeof(float), 64);
+    float *OLDpositionsX = (float*)_aligned_malloc(physics::particleCount * sizeof(float), 64);
+    float *OLDpositionsY = (float*)_aligned_malloc(physics::particleCount * sizeof(float), 64);
 
-    std::vector<dynamics::particle> collective;
-    collective.reserve(particleCount);
-    spawnAlignedParticle(collective, graphics::screenSize/2.0f, particleCount, dynamics::particleRadius);
+    if (positionsX == NULL || positionsY == NULL || OLDpositionsX == NULL || OLDpositionsY == NULL)
+        exit(1);
+
+    SpawnAlignedParticles(positionsX, positionsY, OLDpositionsX, OLDpositionsY);
 
     //PHYSICS LOOP
     while (!WindowShouldClose()) {
         physics::timeAccumulator += GetFrameTime();
 
-        while (physics::timeAccumulator >= physics::physicsTimeStep) {
-            for (int i = 0; i < particleCount; i++) {
-                dynamics::particle& circleData = collective[i];
+        physics::physicsLoop(positionsX, positionsY, OLDpositionsX, OLDpositionsY);
 
-                vec2::Vector2f nextPos = circleData.pos * 2.0 - circleData.oldPos + accel * physics::squarePhysicsStep;
-                circleData.oldPos = circleData.pos;
-                circleData.pos = nextPos;
-            }
-
-            physics::timeAccumulator -= physics::physicsTimeStep;
-        }
-
-
+        graphics::beginDraw();
+        graphics::drawAllParticles();
+        graphics::endDraw();
     }
 
-    
+    _aligned_free(positionsX);
+    _aligned_free(positionsY);
+    _aligned_free(OLDpositionsX);
+    _aligned_free(OLDpositionsY);
 
     return 0;
 };
 
-void spawnAlignedParticle(std::vector<dynamics::particle>& collective, vec2::Vector2f center, int count, float radius) {
-    int side = ceil(sqrtf((float)count));
-    float spacing = radius * 2.0f;
+void SpawnAlignedParticles(std::vector<float>& positionsX, std::vector<float>& positionsY, std::vector<float>& OLDpositionsX, std::vector<float>& OLDpositionsY) {
+    int side = ceil(sqrtf((float)physics::particleCount));
+    float spacing = dynamics::particleRadius * 2.0f;
     float offsetToCenter = (side * spacing) / 2.0f;
 
-    //center = center / 2.0f;
+    vec2::Vector2f center = graphics::screenSize/2;
 
-    for (int i = 0; i < count; ++i) {
+    for (int i = 0; i < physics::particleCount; ++i) {
         int row = i / side;
         int col = i % side;
 
-        dynamics::particle element = dynamics::create();
+        float posX = center.x - offsetToCenter + (col * spacing);
+        float posY = center.y - offsetToCenter + (row * spacing);
 
-        element.pos.x = center.x - offsetToCenter + (col * spacing);
-        element.pos.y = center.y - offsetToCenter + (row * spacing);
-        element.oldPos = element.pos;
-
-        collective.push_back(element);
+        positionsX.push_back(posX);
+        positionsY.push_back(posY);
+        
+        OLDpositionsX.push_back(posX);
+        OLDpositionsY.push_back(posY);
     }
 }
--- particle.hpp
+++ particle.hpp
@@ -2,14 +2,6 @@
 #include "vector2.hpp"
 
 namespace dynamics {
-    struct particle {
-        vec2::Vector2d oldPos;
-        vec2::Vector2d pos;
-        vec2::Vector2d accel;
-    };
-
     inline constexpr Color particleColor = { 192, 192, 192, 255 };
     inline constexpr float particleRadius = 1.0f;
-
-    particle create();
 }
