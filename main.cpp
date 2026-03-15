a39789f4714bb7ef90813a94e74745055c609053
TylerShax <247200418+TylerShax@users.noreply.github.com>
2026-03-15 15:35:57 +0100

    Added physics module

--- main.cpp
+++ main.cpp
@@ -1,68 +1,41 @@
 #include <vector>
-#include "raylib.h"
+#include "graphics.hpp"
 #include "particle.hpp"
+#include "physics.hpp"
 
 void spawnAlignedParticle(std::vector<dynamics::particle>& collective, vec2::Vector2f center, int count, float radius);
 
 int main() {
 
-    // WINDOW CONFIG
-    constexpr unsigned short sizeX = 1440;
-    constexpr unsigned short sizeY = 1280;
-    constexpr unsigned short fpsTarget = 240;
-
-    vec2::Vector2f screenSize = { (float)sizeX, (float)sizeY };
-
-    const Color backgroundColor = {34, 34, 34, 255};
-
-    InitWindow(sizeX, sizeY, "Shax's Physics Engine");
-    SetTargetFPS(fpsTarget);
+    graphics::initWindow();
 
     // PARTICLE INIT
     constexpr short unsigned int particleCount = 100000;
 
     std::vector<dynamics::particle> collective;
     collective.reserve(particleCount);
-    spawnAlignedParticle(collective, screenSize/2.0f, particleCount, radius);
+    spawnAlignedParticle(collective, graphics::screenSize/2.0f, particleCount, dynamics::particleRadius);
 
     //PHYSICS LOOP
-    
-    //constants
-    float gravity = 800.0f;
-    constexpr float physicsTimeStep = 1.0f/480.0f;
-    constexpr float squarePhysicsStep = physicsTimeStep * physicsTimeStep;
-
-    float timeAccumulator = 0.0f;
-    vec2::Vector2f accel = { 0.0f, gravity };
-
     while (!WindowShouldClose()) {
-        timeAccumulator += GetFrameTime();
+        physics::timeAccumulator += GetFrameTime();
 
-        while (timeAccumulator >= physicsTimeStep) {
+        while (physics::timeAccumulator >= physics::physicsTimeStep) {
             for (int i = 0; i < particleCount; i++) {
                 dynamics::particle& circleData = collective[i];
 
-                vec2::Vector2f nextPos = circleData.pos * 2.0 - circleData.oldPos + accel * squarePhysicsStep;
+                vec2::Vector2f nextPos = circleData.pos * 2.0 - circleData.oldPos + accel * physics::squarePhysicsStep;
                 circleData.oldPos = circleData.pos;
                 circleData.pos = nextPos;
             }
 
-            timeAccumulator -= physicsTimeStep;
+            physics::timeAccumulator -= physics::physicsTimeStep;
         }
 
-        BeginDrawing();
-        ClearBackground(backgroundColor);
-        DrawFPS(10, 10);
 
-        for (int i = 0; i < particleCount; i++) {
-            dynamics::particle& circleData = collective[i];
-            DrawCircle((int)circleData.pos.x, (int)circleData.pos.y, radius, particleColor);
-        }
-
-        EndDrawing();
     }
 
-    CloseWindow();
+    
 
     return 0;
 };
